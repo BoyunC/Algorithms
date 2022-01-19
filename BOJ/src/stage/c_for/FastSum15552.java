f4983d9c5b711e2db36823f7296c561fd6978331
BoyunC <cbyn0819@gmail.com>
2022-01-19 19:30:18 +0900

    6. 백준 3단계 for문 문제 15552번 "빠른 A+B" 업로드

---
+++ BOJ/src/stage/c_for/FastSum15552.java
@@ -0,0 +1,29 @@
+package stage.c_for;
+
+import java.io.*;
+import java.util.StringTokenizer;
+
+public class FastSum15552 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        int n = Integer.parseInt(br.readLine());
+
+        int[][] arr = new int[n][2];
+
+        for(int i=0; i<n; i++){
+            StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+            arr[i][0] = Integer.parseInt(st.nextToken());
+            arr[i][1] = Integer.parseInt(st.nextToken());
+        }
+
+        for(int i=0; i<n; i++){
+            bw.write(String.valueOf(arr[i][0]+arr[i][1]));
+            bw.write("\n");
+        }
+
+        bw.flush();
+        bw.close();
+    }
+}
