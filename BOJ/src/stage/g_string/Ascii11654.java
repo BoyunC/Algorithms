0fe8117b53417a37db83548d14ac2e4f16dda36c
BoyunC <cbyn0819@gmail.com>
2022-02-01 23:12:45 +0900

    9. 백준 7단계 문자열 문제 "아스키 코드" 문제 업로드/까치설날

---
+++ BOJ/src/stage/g_string/Ascii11654.java
@@ -0,0 +1,17 @@
+package stage.g_string;
+
+/*
+    문제 11654번 : 아스키 코드
+ */
+
+import java.util.Scanner;
+
+public class Ascii11654 {
+    public static void main(String[] args) {
+        Scanner kb = new Scanner(System.in);
+
+        int a = (int)kb.next().charAt(0);
+
+        System.out.println(a);
+    }
+}
