92caa7d3e83a8e27b188201cc994941f9478ac2d
BoyunC <cbyn0819@gmail.com>
2022-02-18 20:26:14 +0900

    백준 9단계 재귀 피보나치 문제 추가 코드 업로드

--- BOJ/src/stage/i_recursive/Fibonacci10870.java
+++ BOJ/src/stage/i_recursive/Fibonacci10870.java
@@ -23,4 +23,27 @@ public class Fibonacci10870 {
 
         System.out.println(fibonacci(n));
     }
+
+    /*
+        public static int[] fibo;
+
+    public static int fibonacci(int n) {
+        if(fibo[n]>0) return fibo[n];
+        if(n==1) return fibo[n] = 1;
+        else if(n==2) return fibo[n] = 1;
+
+        return fibo[n] = fibonacci(n-2) + fibonacci(n-1);
+    }
+
+    public static void main(String[] args) {
+        Scanner kb = new Scanner(System.in);
+
+        int n = kb.nextInt();
+        fibo = new int[n+1];
+        fibonacci(n);
+        System.out.println(fibonacci(n));
+    }
+     */
+
+
 }
