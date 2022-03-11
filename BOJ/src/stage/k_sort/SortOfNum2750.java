5a9f653bf36dc3a1b28dc0602214d9315ade5a02
BoyunC <cbyn0819@gmail.com>
2022-03-11 22:48:07 +0900

    백준 단계별 문제 중 정렬 "수 정렬하기" 문제 업로드

---
+++ BOJ/src/stage/k_sort/SortOfNum2750.java
@@ -0,0 +1,31 @@
+package stage.k_sort;
+
+/*
+     문제 2750번 : 수 정렬하기
+*/
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Comparator;
+import java.util.Scanner;
+
+public class SortOfNum2750 {
+    public static void main(String[] args) {
+        Scanner kb = new Scanner(System.in);
+
+        int n = kb.nextInt();
+
+        ArrayList<Integer> arr = new ArrayList<>();
+
+        for(int i=0; i<n; i++) {
+            int a = kb.nextInt();
+            if(!arr.contains(a))
+                arr.add(a);
+        }
+
+        arr.sort(Comparator.naturalOrder());
+
+        for(int x: arr)
+            System.out.println(x);
+    }
+}
