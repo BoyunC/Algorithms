326e001ad396eedafb88eba21199f182cfdcd791
BoyunC <cbyn0819@gmail.com>
2022-03-31 00:27:27 +0900

    백준 단계별 문제 중 백트래킹 "스도쿠" 문제 업로드

--- BOJ/src/stage/l_backtracking/Sudoku2580.java
+++ BOJ/src/stage/l_backtracking/Sudoku2580.java
@@ -5,7 +5,7 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.StringTokenizer;
 
-public class sudoku2580 {
+public class Sudoku2580 {
 
     static int[][] arr = new int[9][9];
     public static StringBuilder builder = new StringBuilder();
@@ -13,15 +13,55 @@ public class sudoku2580 {
     public static void dfs(int row, int col) {
         if(col==9) {
             dfs(row+1, 0);
-        } else if(row==9) {
+        }
+
+        if(row==9) {
             for(int i=0; i<9; i++){
                 for(int j=0; j<9; j++) {
                     builder.append(arr[i][j] + " ");
                 }
                 builder.append("\n");
+                return;
             }
         }
 
+        if(arr[row][col] == 0) {
+            for(int i=1; i<9; i++){
+                if(check(row, col, i)) {
+                    arr[row][col] = i;
+                    dfs(row, col+1);
+                }
+            }
+            arr[row][col] = 0;
+            return;
+        }
+
+        dfs(row, col+1);
+    }
+
+    public static boolean check(int row, int col, int value) {
+
+        for(int i=0 ; i<9 ; i++) {
+            if(arr[row][i] == value)
+                return false;
+        }
+
+        for(int i=0 ; i<9 ; i++) {
+            if(arr[i][col] == value)
+                return false;
+        }
+
+        int in_row = (row/3)*3;
+        int in_col = (col/3)*3;
+
+        for(int i=in_row ; i<in_row+3 ; i++) {
+            for(int j=in_col ; j<in_col+3 ; j++) {
+                if(arr[i][j]==value)
+                    return false;
+            }
+        }
+
+        return true;
     }
 
     public static void main(String[] args) throws IOException {
