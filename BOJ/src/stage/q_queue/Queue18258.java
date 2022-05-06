03b763529e57ed7c5a80ead931d0626a4a15f70a
BoyunC <cbyn0819@gmail.com>
2022-05-07 00:43:46 +0900

    백준 단계별 문제 중 스택 "큐2" 문제 업로드

---
+++ BOJ/src/stage/q_queue/Queue18258.java
@@ -0,0 +1,78 @@
+package stage.q_queue;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Deque;
+import java.util.LinkedList;
+import java.util.StringTokenizer;
+
+public class Queue18258 {
+    public static void main(String[] args) throws IOException {
+
+        BufferedReader kb = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+
+        Deque<Integer> queue = new LinkedList<>();
+
+        int n = Integer.parseInt(kb.readLine());
+
+        while(n-->0) {
+            StringTokenizer st = new StringTokenizer(kb.readLine(), " ");
+
+            switch (st.nextToken()) {
+                case "push" :
+                    queue.offer(Integer.parseInt(st.nextToken()));
+                    break;
+
+                case "pop" :
+                    Integer num = queue.poll();
+
+                    if(num == null)
+                        sb.append(-1);
+                    else
+                        sb.append(num);
+
+                    sb.append("\n");
+                    break;
+
+                case "size" :
+                    sb.append(queue.size()).append("\n");
+                    break;
+
+                case "empty" :
+                    if(queue.isEmpty())
+                        sb.append(1);
+                    else
+                        sb.append(0);
+
+                    sb.append("\n");
+                    break;
+
+                case "front" :
+                    Integer num2 = queue.peek();
+
+                    if(num2 == null)
+                        sb.append(-1);
+                    else
+                        sb.append(num2);
+
+                    sb.append("\n");
+                    break;
+
+                case "back" :
+                    Integer num3 = queue.peekLast();
+
+                    if(num3 == null)
+                        sb.append(-1);
+                    else
+                        sb.append(num3);
+
+                    sb.append("\n");
+                    break;
+            }
+        }
+
+        System.out.println(sb);
+    }
+}
