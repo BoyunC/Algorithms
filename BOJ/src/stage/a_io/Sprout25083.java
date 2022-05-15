72fee69b89e3b7590c0b2763924fcd5cd4e6f970
BoyunC <cbyn0819@gmail.com>
2022-05-16 01:07:53 +0900

    백준 단계별 문제 중 분할정복 "색종이" 문제 업로드

---
+++ BOJ/src/stage/a_io/Sprout25083.java
@@ -0,0 +1,12 @@
+package stage.a_io;
+
+public class Sprout25083 {
+    public static void main(String[] args) {
+        System.out.printf("         ,r'\"7\n");
+        System.out.printf("r`-_   ,'  ,/\n");
+        System.out.printf(" \\. \". L_r'\n");
+        System.out.printf("   `~\\/\n");
+        System.out.printf("      |\n");
+        System.out.printf("      |\n");
+    }
+}
