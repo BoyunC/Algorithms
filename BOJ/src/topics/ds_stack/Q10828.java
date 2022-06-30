92432b9cd505198c95a0ce762a45b7b5c0e3735c
BoyunC <cbyn0819@gmail.com>
2022-07-01 01:10:18 +0900

    [종류별] : 스택 -> Q.10828 스택 문제 업로드

---
+++ BOJ/src/topics/ds_stack/Q10828.java
@@ -0,0 +1,57 @@
+package topics.ds_stack;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Q10828 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader kb = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+
+        int n = Integer.parseInt(kb.readLine());
+        int[] stack = new int[n];
+
+        int size = 0;
+
+        while(n-->0) {
+            StringTokenizer st = new StringTokenizer(kb.readLine(), " ");
+
+            switch (st.nextToken()) {
+                case "push":
+                    stack[size] = Integer.parseInt(st.nextToken());
+                    size++;
+                    break;
+                case "pop" :
+                    if(size==0)
+                        sb.append(-1);
+                    else {
+                        int result = stack[size-1];
+                        stack[size-1] = 0;
+                        sb.append(result);
+                        size--;
+                    }
+                    sb.append("\n");
+                    break;
+                case "size":
+                    sb.append(size).append("\n");
+                    break;
+                case "empty":
+                    if(size==0)
+                        sb.append(1).append("\n");
+                    else
+                        sb.append(0).append("\n");
+                    break;
+                case "top":
+                    if(size==0)
+                        sb.append(-1).append("\n");
+                    else
+                        sb.append(stack[size-1]).append("\n");
+                    break;
+            }
+        }
+
+        System.out.println(sb);
+    }
+}
