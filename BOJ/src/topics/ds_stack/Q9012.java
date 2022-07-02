484f565aa7261d6aebc31ca71fd32014ec3eb767
BoyunC <cbyn0819@gmail.com>
2022-07-02 11:29:11 +0900

    [종류별] : 스택 -> Q.9012 괄호 문제 업로드

---
+++ BOJ/src/topics/ds_stack/Q9012.java
@@ -0,0 +1,44 @@
+package topics.ds_stack;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Stack;
+
+public class Q9012 {
+    public static String solution(String str){
+        Stack<Character> stack = new Stack<>();
+
+        for(int i = 0; i < str.length(); i++){
+            char c = str.charAt(i);
+
+            if(c == '(')
+                stack.push(c);
+            else if(stack.empty())
+                return "NO";
+            else
+                stack.pop();
+        }
+
+        if (stack.empty()) {
+            return "YES";
+        } else {
+            return "NO";
+        }
+    }
+
+
+    public static void main(String[] args) throws IOException {
+
+        BufferedReader kb = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+
+        int t = Integer.parseInt(kb.readLine());
+
+        for (int i = 0; i < t; i++) {
+            sb.append(solution(kb.readLine())).append('\n');
+        }
+
+        System.out.println(sb);
+    }
+}
