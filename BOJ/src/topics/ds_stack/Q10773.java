bfd0183175e66374992b65061d528f423ad06b1e
BoyunC <cbyn0819@gmail.com>
2022-07-03 20:33:51 +0900

    [종류별] : 스택 -> Q.10773 제로 문제 업로드

---
+++ BOJ/src/topics/ds_stack/Q10773.java
@@ -0,0 +1,35 @@
+package topics.ds_stack;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Stack;
+
+public class Q10773 {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader kb = new BufferedReader(new InputStreamReader(System.in));
+
+        Stack<Integer> stack = new Stack<>();
+
+        int k = Integer.parseInt(kb.readLine());
+
+        for(int i = 0; i < k; i++) {
+            int num = Integer.parseInt(kb.readLine());
+
+            if(num == 0) {
+                stack.pop();
+            } else {
+                stack.add(num);
+            }
+        }
+
+        int sum = 0;
+
+        for(int x : stack) {
+            sum += x;
+        }
+
+        System.out.println(sum);
+    }
+}
