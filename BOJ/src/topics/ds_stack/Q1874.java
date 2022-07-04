7bbcae4f12365357754dbd3f09d0b8d3d474fb7d
BoyunC <cbyn0819@gmail.com>
2022-07-04 22:10:09 +0900

    [종류별] : 스택 -> Q.1874 스택 수열 문제 업로드

---
+++ BOJ/src/topics/ds_stack/Q1874.java
@@ -0,0 +1,40 @@
+package topics.ds_stack;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Stack;
+
+public class Q1874 {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader kb = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+        Stack<Integer> stack = new Stack<>();
+
+        int n = Integer.parseInt(kb.readLine());
+        int start = 0;
+
+        while (n --> 0){
+            int num = Integer.parseInt(kb.readLine());
+
+            if(num > start) {
+                for(int i = start + 1; i <= num; i++) {
+                    stack.push(i);
+                    sb.append('+').append('\n');
+                }
+
+                start = num;
+            } else if(stack.peek() != num){
+                System.out.println("NO");
+
+                return;
+            }
+
+            stack.pop();
+            sb.append('-').append('\n');
+        }
+
+        System.out.println(sb);
+    }
+}
