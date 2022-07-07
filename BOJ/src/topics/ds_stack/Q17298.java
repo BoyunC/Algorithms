9b91d8dbfee27d8b96f28681461369e688f3073f
BoyunC <cbyn0819@gmail.com>
2022-07-07 21:51:01 +0900

    [종류별] : 스택 -> Q.17298 오큰수 문제 업로드

---
+++ BOJ/src/topics/ds_stack/Q17298.java
@@ -0,0 +1,42 @@
+package topics.ds_stack;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Stack;
+import java.util.StringTokenizer;
+
+public class Q17298 {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader kb = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+        Stack<Integer> stack = new Stack<>();
+
+        int n = Integer.parseInt(kb.readLine());
+        int[] sequence = new int[n];
+
+        StringTokenizer st = new StringTokenizer(kb.readLine(), " ");
+        for(int i = 0; i < n; i++) {
+            sequence[i] = Integer.parseInt(st.nextToken());
+        }
+
+        for(int i = 0; i < n; i++) {
+            while (!stack.isEmpty() && sequence[stack.peek()] < sequence[i]) {
+                sequence[stack.pop()] = sequence[i];
+            }
+
+            stack.push(i);
+        }
+
+        while (!stack.isEmpty()) {
+            sequence[stack.pop()] = -1;
+        }
+
+        for(int i = 0; i < n; i++) {
+            sb.append(sequence[i]).append("\n");
+        }
+
+        System.out.println(sb);
+    }
+}
