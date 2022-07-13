dc8c7efc29c18a67c4a2981e01bc598fd3530791
BoyunC <cbyn0819@gmail.com>
2022-07-13 22:10:49 +0900

    [종류별] : 큐 -> Q.2164 카드2 문제 업로드

---
+++ BOJ/src/topics/ds_queue/Q2164.java
@@ -0,0 +1,29 @@
+package topics.ds_queue;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.LinkedList;
+import java.util.Queue;
+
+public class Q2164 {
+
+    public static void main(String[] args) throws IOException {
+
+        BufferedReader kb = new BufferedReader(new InputStreamReader(System.in));
+
+        Queue<Integer> queue = new LinkedList<>();
+
+        int n = Integer.parseInt(kb.readLine());
+
+        for(int i=1; i<=n; i++)
+            queue.offer(i);
+
+        while (queue.size()>1){
+            queue.poll();
+            queue.offer(queue.poll());
+        }
+
+        System.out.println(queue.poll());
+    }
+}
