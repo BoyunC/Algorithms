f33c376abd0524c820bae8a98faac96f752312bd
BoyunC <cbyn0819@gmail.com>
2022-07-14 21:10:48 +0900

    [종류별] : 큐 -> Q.1966 프린터 큐 문제 업로드

---
+++ BOJ/src/topics/ds_queue/Q1966.java
@@ -0,0 +1,58 @@
+package topics.ds_queue;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.LinkedList;
+import java.util.StringTokenizer;
+
+public class Q1966 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader kb = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+
+        int t = Integer.parseInt(kb.readLine());
+        while (t --> 0) {
+
+            StringTokenizer st = new StringTokenizer(kb.readLine());
+            LinkedList<int[]> queue = new LinkedList<>();
+
+            int n = Integer.parseInt(st.nextToken());
+            int m = Integer.parseInt(st.nextToken());
+
+            st = new StringTokenizer(kb.readLine());
+
+            for (int i = 0; i < n; i++) {
+                queue.offer(new int[]{i, Integer.parseInt(st.nextToken())});
+            }
+
+            int count = 0;
+
+            while (!queue.isEmpty()) {
+
+                int[] front = queue.poll();
+                boolean isMax = true;
+
+                for(int i = 0; i < queue.size(); i++) {
+                    if(front[1] < queue.get(i)[1]) {
+                        queue.offer(front);
+                        for(int j = 0; j < i; j++) {
+                            queue.offer(queue.poll());
+                        }
+                        isMax = false;
+                        break;
+                    }
+                }
+                if(isMax == false) {
+                    continue;
+                }
+                count++;
+                if(front[0] == m) {
+                    break;
+                }
+            }
+            sb.append(count).append("\n");
+        }
+        System.out.println(sb);
+    }
+}
